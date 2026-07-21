5f81705a9d923cb5f41af7dd03987891f973313e
vaishnavipatil2469-commits <vaishnavipatil2469@gmail.com>
2026-07-21 09:44:57 +0000

    changes

---
+++ large.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    if (a > b)
+        cout << "Largest = " << a;
+    else
+        cout << "Largest = " << b;
+
+    return 0;
+}

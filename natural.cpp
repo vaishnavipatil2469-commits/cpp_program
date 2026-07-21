88fd28192282303066aca910ca3a831c7d14adce
vaishnavipatil2469-commits <vaishnavipatil2469@gmail.com>
2026-07-21 10:06:17 +0000

    changes

---
+++ natural.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    while (i <= n) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}

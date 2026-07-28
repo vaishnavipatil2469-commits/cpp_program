f98013e51f7843f505a757298bff3c3c9a48f7ca
vaishnavipatil2469-commits <vaishnavipatil2469@gmail.com>
2026-07-28 08:53:27 +0000

    changes

---
+++ sub.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"enter the value of n :";
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n+1;i++)
+    sum=sum+i;
+    cout<<sum;
+}
